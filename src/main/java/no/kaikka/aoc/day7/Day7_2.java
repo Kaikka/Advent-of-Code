5c6b2cbf1bb032b39c30d405290edc6f85ef8fac
Kaikka <kaileeto@gmail.com>
2024-12-07 22:19:11 +0100

    day 7 part 2. slightly edit part 1 code -> done

---
+++ src/main/java/no/kaikka/aoc/day7/Day7_2.java
@@ -0,0 +1,38 @@
+package no.kaikka.aoc.day7;
+
+import no.kaikka.aoc.utils.AOCUtils;
+
+import java.util.Arrays;
+
+public class Day7_2 {
+    public static void main(String[] args) {
+        var start = System.currentTimeMillis();
+        String[] input = AOCUtils.getInput(7).split("\n");
+
+        long counter = 0;
+
+        for (String s : input) {
+            int splitter = s.indexOf(':');
+            long goal = Long.parseLong(s.substring(0, splitter));
+            long[] numbers = Arrays.stream(s.substring(splitter + 1).trim().split(" ")).mapToLong(Long::parseLong).toArray();
+
+            if (solve(numbers, 1, goal, numbers[0])) counter += goal;
+        }
+
+        System.out.println("Part 2: " + counter);
+        var end = System.currentTimeMillis();
+        System.out.println("Runtime: " + (end - start) + "ms");
+    }
+
+    private static boolean solve(long[] values, int index, long goal, long n) {
+        if (n == goal && index == values.length) return true;
+        if (index >= values.length || n > goal) return false;
+
+        long m = values[index];
+        long added = n + m;
+        long multiplied = n * m;
+        long concatinated = Long.parseLong("" + n + m);
+
+        return solve(values, index+1, goal, added) || solve(values, index+1, goal, multiplied) || solve(values, index+1, goal, concatinated);
+    }
+}
