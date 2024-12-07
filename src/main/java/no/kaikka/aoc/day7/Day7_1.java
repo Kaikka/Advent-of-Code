0b6089942d38bb62ee755a358729c652c6b61616
Kaikka <kaileeto@gmail.com>
2024-12-07 22:09:25 +0100

    day 7 part 1. Bug was a case like "Goal: 20, numbers: 10 2 9". This would return true because 10*2=20, but we needed to include 9. Reading is hard

--- src/main/java/no/kaikka/aoc/day7/Day7_1.java
+++ src/main/java/no/kaikka/aoc/day7/Day7_1.java
@@ -14,7 +14,7 @@ public class Day7_1 {
         for (String s : input) {
             int splitter = s.indexOf(':');
             long goal = Long.parseLong(s.substring(0, splitter));
-            int[] numbers = Arrays.stream(s.substring(splitter + 1).trim().split(" ")).mapToInt(Integer::parseInt).toArray();
+            long[] numbers = Arrays.stream(s.substring(splitter + 1).trim().split(" ")).mapToLong(Long::parseLong).toArray();
 
             if (solve(numbers, 1, goal, numbers[0])) counter += goal;
         }
@@ -24,11 +24,11 @@ public class Day7_1 {
         System.out.println("Runtime: " + (end - start) + "ms");
     }
 
-    private static boolean solve(int[] values, int index, long goal, long n) {
-        if (n == goal) return true;
+    private static boolean solve(long[] values, int index, long goal, long n) {
+        if (n == goal && index == values.length) return true;
         if (index >= values.length || n > goal) return false;
 
-        int m = values[index];
+        long m = values[index];
         long added = n + m;
         long multiplied = n * m;
 
