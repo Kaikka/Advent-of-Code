9b280a98093a07891cf54c58996454e08a0139b6
Kaikka <kaileeto@gmail.com>
2025-12-01 22:13:28 +0100

    AOCRunner abstract class for easier tracking etc

---
+++ src/main/java/no/kaikka/aoc/y2025/AOCRunner.java
@@ -0,0 +1,25 @@
+package no.kaikka.aoc.y2025;
+
+import no.kaikka.aoc.utils.AOCUtils;
+
+public abstract class AOCRunner {
+    public void run() {
+        String input = AOCUtils.getInput();
+        var startP1 = System.nanoTime();
+        String part1 = part1(input);
+        var endP1 = System.nanoTime();
+        var startP2 = System.nanoTime();
+        String part2 = part2(input);
+        var endP2 = System.nanoTime();
+
+        System.out.println("Part 1: " + part1);
+        System.out.println("Part 2: " + part2);
+
+        System.out.printf("Part 1 runtime: %.2f ms%n", (endP1 - startP1) / 1_000_000.0);
+        System.out.printf("Part 2 runtime: %.2f ms%n", (endP2 -  startP2) / 1_000_000.0);
+
+    }
+
+    public abstract String part1(String input);
+    public abstract String part2(String input);
+}
--- src/main/java/no/kaikka/aoc/y2025/Day1_1.java
+++ src/main/java/no/kaikka/aoc/y2025/Day1_1.java
@@ -1,21 +1,18 @@
 package no.kaikka.aoc.y2025;
 
-import no.kaikka.aoc.utils.AOCUtils;
-
-public class Day1_1 {
-
-    public enum Direction {
-        LEFT,
-        RIGHT
-    }
+public class Day1_1 extends AOCRunner {
 
     public static void main(String[] args) {
-        var start = System.nanoTime();
-        var input = AOCUtils.getInput().split("\\s");
+        new Day1_1().run();
+    }
+
+    @Override
+    public String part1(String input) {
+        var input2 = input.split("\\s");
         int dialPointValue = 50;
         int timesAtZero = 0;
 
-        for (String line : input) {
+        for (String line : input2) {
             Direction dir = getDirection(line.charAt(0));
             int n = Integer.parseInt(line.substring(1));
             n = n % 100; // No point in rotating multiple times
@@ -35,11 +32,17 @@ public class Day1_1 {
 
             if (dialPointValue == 0) timesAtZero++;
         }
+        return "" + timesAtZero;
+    }
 
-        var end = System.nanoTime();
-        System.out.printf("Runtime: %.2f ms%n", (end - start) / 1_000_000.0);
-        System.out.println(timesAtZero);
+    @Override
+    public String part2(String input) {
+        return "Not yet implemented";
+    }
 
+    public enum Direction {
+        LEFT,
+        RIGHT
     }
 
     private static Direction getDirection(char c) {
